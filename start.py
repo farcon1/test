c84573627b42736114a88d7adb754eb27fd74e79
farcon1 <66922506+farcon1@users.noreply.github.com>
2020-06-17 18:39:07 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -5,5 +5,5 @@ from openpyxl import load_workbook
 wb = openpyxl.load_workbook(filename = "test1/ex.xlsx")
 sheet = wb['my_sheet']    
 sheet["A1"].value="12345678910"
-wb.save()
+wb.save("test1/ex.xlsx")
 print(sheet["A1"].value)
